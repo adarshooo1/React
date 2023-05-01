65365740fdf06742db3094ec41ffc8e07e5d2b4f
Adarsh Singh <adarshsinghak001@gmail.com>
2023-05-01 22:36:30 +0530

    React Day 3 Added

--- React + Vite Part 3/vite-project/src/App.jsx
+++ React + Vite Part 3/vite-project/src/App.jsx
@@ -1,19 +1,43 @@
 import './App.css'
 import Video from './Components/Video'
-import Image from './Components/Image'
+
 
 // How to import multiple funciton from a single component file.
 // import { Video  , Thumbnail} from './Components/Video'
 
 
 function app() {
+  let topic = "React js"
+  let className = "Message"
   return (
-    <div>
-      <div>Hello</div>
-      <Video></Video>
-      <Image></Image>
-      {/* <Thumbnail></Thumbnail> */}
-    </div>
+      <>
+      
+      <div className='App'>
+        <div>Video</div>
+
+        <div className='Video'>
+          <Video
+          title="React JS Tutorial"
+          Views="10k"
+          time="1 yearAgo"
+          channel="Adarsh Singh">
+          </Video>
+
+          <Video title="Node JS Tutorial"
+          Views="100k"
+          time="3
+          year Ago"
+          channel="WebDev">
+          </Video>
+
+          <Video title="Mongo db"
+          Views="120k"
+          time="5 year Ago"
+          channel="Code Bros">
+          </Video>
+        </div>
+      </div>
+    </>
   )
 
 
--- React + Vite Part 3/vite-project/src/Components/Video.jsx
+++ React + Vite Part 3/vite-project/src/Components/Video.jsx
@@ -1,18 +1,18 @@
-function Video() {
+import './Video.css';
 
-    return <div>This is a youtube Vedio</div>
+export default function Video({title , channel , Views , time}){
+    return(
+    <>
+    <div className='Container'>
+    <div className='pic'>
+    <img src="https://loremflickr.com/200/160/wallpaper" alt="RandomImage" />
+    </div>
+    <div className='title'>{title}</div>
+    <div className='channel'>{channel}</div>
+    <div className='views'>
+        {Views} Views <span>.</span> {time} </div>
+    </div>
 
-}
-
-
-// function Thumbnail(){
-//      return <div>Thumbnail</div>
-// }
-
-// If we have multiple things in a single component file.
-// export {Video , Thumbnail};
-
-export default Video;
-
-// NOTE: As per the react rules Every component have their own component file like Here vedio have the own component file;
-//Each and every js file  should be end from .jsx extension if javaScript File and .tsx extension of typeScript file, Not the js or ts extension;
+    </>
+    )
+} 
