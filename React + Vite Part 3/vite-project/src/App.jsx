f5f758bf20bfccb841e678c213f20e046b49fc78
Adarsh Singh <adarshsinghak001@gmail.com>
2023-05-02 22:26:33 +0530

    React Day 4 Added

--- React + Vite Part 3/vite-project/src/App.jsx
+++ React + Vite Part 3/vite-project/src/App.jsx
@@ -7,42 +7,62 @@ import Video from './Components/Video'
 
 
 function app() {
-  let topic = "React js"
-  let className = "Message"
+
+  let videos = [{
+    id : 1,
+    title: 'React Js tutorial',
+    views: '10k',
+    time: '1 year ago',
+    channel: "Let's Code In",
+    verified : false,
+},
+{
+  id : 2,
+  title: 'Node Js tutorial',
+  views: '100k',
+  time: '2 year ago',
+  channel: "Mocha Coding",
+  verified : true,
+},
+{
+  id : 3,
+  title: 'Life Style',
+  views: '1M',
+  time: '1  months ago',
+  channel: "5 Min hack",
+  verified : false,
+}];
+
+
   return (
       <>
-      
       <div className='App'>
         <div>Video</div>
 
         <div className='Video'>
-          <Video
-          title="React JS Tutorial"
-          Views="10k"
-          time="1 yearAgo"
-          channel="Adarsh Singh">
-          </Video>
+          {
+            videos.map(video =><Video
+                  title={video.title}
+                  views={video.views}
+                  time={video.time}
+                  channel={video.channel}
+                  verified={video.verified}
+             ></Video>
+            )
+          }
 
-          <Video title="Node JS Tutorial"
-          Views="100k"
-          time="3
-          year Ago"
-          channel="WebDev">
-          </Video>
-
-          <Video title="Mongo db"
-          Views="120k"
+          {/* <Video
+          verified={true}
+          title="Mongo db"
+          views="120k"
           time="5 year Ago"
           channel="Code Bros">
-          </Video>
+          </Video> */}
         </div>
       </div>
     </>
   )
 
-
 }
-
-
 
 export default app
