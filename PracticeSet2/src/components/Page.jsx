eb9c05ad6d0bd8fc8b1bcf3db72997574c713d90
Adarsh Singh <adarshsinghak001@gmail.com>
2023-06-06 23:44:26 +0530

    React Day 4 Assignment Day 1

--- PracticeSet2/src/components/Page.jsx
+++ PracticeSet2/src/components/Page.jsx
@@ -1,16 +1,62 @@
 import React from "react";
-import "./Page.css"
+import "./Page.css";
 
 function Page() {
+  function handleClick(e) {
+    e.preventDefault();
+  }
+
+  function handleMouseOver() {
+    console.log("Mouse is over the image");
+  }
+
+  function handleMouseOut() {
+    console.log("Mouse is out of the image");
+  }
+
+  function focus() {
+    console.log("Focused on the text box");
+  }
+
+  function blur() {
+    console.log("Blur on the text box");
+  }
+
+  const cut = (e) => {
+    console.log(e.target.value);
+  };
+
+  const copy = (e) => {
+    console.log(e.target.value);
+  };
+
+  const paste = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <div className="Container">
       <form action="#">
         <div className="ImageContainer">
-          <img src="https://loremflickr.com/420/340" />
+          <img
+            src="https://loremflickr.com/420/340"
+            className="Image"
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          />
         </div>
         <div className="Holders">
-          <input type="text" placeholder="Enter your text" />
-          <button type="submit">Submit</button>
+          <input
+            onCut={cut}
+            onCopy={copy}
+            onPaste={paste}
+            onFocus={focus}
+            onBlur={blur}
+            type="text"
+            placeholder="Enter your text"
+          />
+          <button onClick={handleClick} type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
